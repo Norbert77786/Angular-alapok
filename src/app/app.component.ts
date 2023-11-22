c3e1a30c0c5bfe22b5ec4159d237fb03e6ba2f91
Norbert77786 <tokenorbert7@gmail.com>
2023-11-22 10:45:25 +0100

    Gyakorlás

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MessagesService } from './services/messages.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [MessagesService]
 })
-export class AppComponent {
-  title = 'projekt';
+export class AppComponent implements OnInit {
+
+  //alapok
+  title: string='1.Introduction'
+  myBtn: string='My Button'
+  counter: number=0
+
+  //gomb kikapcsolása
+  isDisabled: boolean=false
+  angularImage: string=''
+  
+  //stílus
+  bgColor: string= 'blue'
+  titleColor: string='white'
+  description: string='font-size:30px; color:lightblue'
+  myButtonbgColor: string='background-color:green'
+
+  //osztály
+  redText: boolean=true
+ 
+
+  incrementCounter(){
+    this.counter++
+  }
+
+  inputText: string="Initial Value"
+
+  //ngOsztályok
+  message: string='This is a dangerous message'
+  classes: string='danger text-size'
+
+  //ngStílusok
+  selectedColor: string='red'
+
+ //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  isLoggedIn: boolean=true
+  username:string='Umair'
+
+  names: string[]=['umair', 'ali', 'john']
+  grade: string='A'
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  today: number= Date.now()
+  currency: number=1.3465
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  messages: string[]=[]
+  posts: any[]=[]
+
+  constructor(private messagesService: MessagesService) {
+    this.messages=messagesService.getMessages()
+  }
+
+  ngOnInit() {
+    this.messagesService.getPosts().subscribe(Response=>{
+      this.posts=Response
+    }, (error)=>{
+      console.error(error)
+    });
+    
+  }
+
+
 }
---
+++ src/app/services/messages.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessagesService {
+
+  private apiUrl='https://jsonplaceholder.typicode.com/posts'
+  constructor(private http:HttpClient) { }
+
+  getMessages() {
+    return['message1', 'message2', 'message3']
+  }
+
+  getPosts(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl)
+  }
+}

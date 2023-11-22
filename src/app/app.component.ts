5dae22035d8c5f2c66acd048a1bf40ee22fead6f
toketamas <toketamas@gmail.com>
2023-11-22 12:52:34 +0100

    - decrement button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,72 +5,76 @@ import { MessagesService } from './services/messages.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [MessagesService]
+  providers: [MessagesService],
 })
 export class AppComponent implements OnInit {
-
   //alapok
-  title: string='1.Introduction'
-  myBtn: string='My Button'
-  counter: number=0
+  title: string = '1.Introduction';
+  myBtn: string = 'My Button';
+  counter: number = 0;
 
   //gomb kikapcsolása
-  isDisabled: boolean=false
-  angularImage: string=''
-  
+  isDisabled: boolean = false;
+  angularImage: string = '';
+
   //stílus
-  bgColor: string= 'blue'
-  titleColor: string='white'
-  description: string='font-size:30px; color:lightblue'
-  myButtonbgColor: string='background-color:green'
+  bgColor: string = 'blue';
+  titleColor: string = 'white';
+  description: string = 'font-size:30px; color:lightblue';
+  myButtonbgColor: string = 'background-color:green';
+  maxWidth: string = '20px';
 
   //osztály
-  redText: boolean=true
- 
+  redText: boolean = false;
 
-  incrementCounter(){
-    this.counter++
+  incrementCounter() {
+    this.counter++;
   }
 
-  inputText: string="Initial Value"
+  decrementCounter() {
+    if (this.counter > 0) {
+      this.counter--;
+    }
+  }
+  inputText: string = 'Initial Value';
 
   //ngOsztályok
-  message: string='This is a dangerous message'
-  classes: string='danger text-size'
+  message: string = 'This is a dangerous message';
+  classes: string = 'danger text-size';
 
   //ngStílusok
-  selectedColor: string='red'
-
- //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-  isLoggedIn: boolean=true
-  username:string='Umair'
-
-  names: string[]=['umair', 'ali', 'john']
-  grade: string='A'
+  selectedColor: string = 'red';
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  today: number= Date.now()
-  currency: number=1.3465
+  isLoggedIn: boolean = false;
+  username: string = 'Umair';
+
+  names: string[] = ['umair', 'ali', 'john'];
+  grade: string = 'A';
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  messages: string[]=[]
-  posts: any[]=[]
+  today: number = Date.now();
+  currency: number = 1.3465;
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  messages: string[] = [];
+  posts: any[] = [];
 
   constructor(private messagesService: MessagesService) {
-    this.messages=messagesService.getMessages()
+    this.messages = messagesService.getMessages();
   }
 
   ngOnInit() {
-    this.messagesService.getPosts().subscribe(Response=>{
-      this.posts=Response
-    }, (error)=>{
-      console.error(error)
-    });
-    
+    this.messagesService.getPosts().subscribe(
+      (Response) => {
+        this.posts = Response;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
   }
-
-
 }
--- src/app/pipes/append.pipe.ts
+++ src/app/pipes/append.pipe.ts
@@ -6,7 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class AppendPipe implements PipeTransform {
 
   transform(value: string, appendText: string): string {
-    if(value==null) return value
+    if(value==null) {
+      return value
+    }
     return value + appendText
   }
 
